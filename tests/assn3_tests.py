44af2366a2c35463adc5d2f1c2128c376feca647
omer.yanay <omeryanay753@gmail.com>
2024-07-10 16:33:07 +0300

    add docker compose

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -7,7 +7,7 @@ books = [
     {"title": "Adventures of Huckleberry Finn", "ISBN": "9780520343641", "genre": "Fiction"},
     {"title": "The Best of Isaac Asimov", "ISBN": "9780385050784", "genre": "Science Fiction"},
     {"title": "Fear No Evil", "ISBN": "9780394558783", "genre": "Biography"},
-    {"title": "No such book", "ISBN": "000000111111", "genre": "Biography"},
+    {"title": "No such book", "ISBN": "0000001111111", "genre": "Biography"},
     {"title": "The Greatest Joke Book Ever", "authors": "Mel Greene", "ISBN": "9780380798490", "genre": "Jokes"}
 ]
 
